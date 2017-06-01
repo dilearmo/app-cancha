3078f149521607e8b6ddf6f5213310f2167e3783
DiLeArMo <DiLeArMo@hotmail.com>
2017-06-01 20:39:21 +0000

    Prueba registro FB debuging

--- js/login.js
+++ js/login.js
@@ -18,6 +18,14 @@
 var base_url = "https://cancha-la-primavera-dilearmo.c9users.io/index.php/";
 
 $(document).ready(function() {
+    facebookConnectPlugin.browserInit('1061872230622284');
+    facebookConnectPlugin.login('email, public_profile', function(response) {
+        toastr.info('status ' + response.status);
+    }, function() {
+        toastr.info('status ' + response.status);
+    });
+    
+    
     if (typeof(Storage) !== "undefined") {
         var nombreUsuario = localStorage.getItem('NombreUsuario');
         if(nombreUsuario != undefined && nombreUsuario != "") {
